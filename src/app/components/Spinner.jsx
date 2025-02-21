c0631a87412e085a6820da2730d7410857f7f6f0
kxvich <kovich112@gmail.com>
2025-02-21 20:28:07 +0100

    initial commit

---
+++ src/app/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import styles from "@/app/styles/Spinner.module.scss";
+
+function Spinner() {
+	return <div className={styles.Spinner}></div>;
+}
+
+export default Spinner;
---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import "./globals.scss";
+
+export const metadata = {
+	title: "AI Text Processor",
+	description: "A simple AI text processor",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<head>
+			<meta
+					httpEquiv="origin-trial"
+					content="Aoeg49e8gXziww8aMaciOT3ocfAg14TCdd6srBr0/ENCVaog72otR4Or4Qjz9qByZNGl2mbK/pxvft9j0jf8sw0AAABReyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDB9"
+				/>
+				<meta
+					httpEquiv="origin-trial"
+					content="ApywZEcawPu3bp6OLLTdoGZKtPjN5sKcNOYQ7FrAJbcOp/vfx7SNIZu8Zxj9gqcIPXzkGd5/KiS1HpvUvKee5gwAAABVeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiQUlTdW1tYXJpemF0aW9uQVBJIiwiZXhwaXJ5IjoxNzUzMTQyNDAwfQ=="
+				/>
+
+				<meta
+					httpEquiv="origin-trial"
+					content="AlvnQOgXEaDkm1KTvW3ZasTnP5EAdLCnhbhfTzwAE2D5V1t2jyJ3+jjnQWgXOtgO40FeJ2rt7V69DIsxHW/7uA4AAABXeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiTGFuZ3VhZ2VEZXRlY3Rpb25BUEkiLCJleHBpcnkiOjE3NDk1OTk5OTl9"
+				/>
+				<meta
+					httpEquiv="Permissions-Policy"
+					content="language-detection=(self)"
+				/>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+}
